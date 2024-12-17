670e4480cf397b12cec972bc2d6dbd3c2ae3b856
Xuyang <xuyang.wu.dev@gmail.com>
2024-12-17 23:48:08 +0000

    change pdf-viewer for custom operations.

--- frontend/src/components/Chatwithpaper/PdfViewer.jsx
+++ frontend/src/components/Chatwithpaper/PdfViewer.jsx
@@ -1,16 +1,29 @@
 import { MyContext } from "@/App";
 import { useContext } from "react";
+import { Worker, Viewer } from "@react-pdf-viewer/core";
+// import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
 
 export default function PdfViewer() {
   const { pdfUrl } = useContext(MyContext);
+  // const pdfUrl = "/ca4_5.pdf";
+  console.log("inpdfview"); 
+  console.log(pdfUrl); 
+  // const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
-  if (!pdfUrl) {
-    return <div>Please upload a PDF file</div>;
-  }
 
   return (
-    <div className="h-full">
-      <embed src={pdfUrl} type="application/pdf" className="w-full h-full" />
-    </div>
-  );
+
+    <Worker workerUrl="/pdf.worker.min.js">
+            <Viewer
+                fileUrl={pdfUrl}
+                // plugins={[defaultLayoutPluginInstance]}
+            />
+        </Worker>
+
+   
+);
 }
